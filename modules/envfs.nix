ff6d7719755c51be5d5685e1fc7faf82e7d8ef34
Lily Foster <lily@lily.flowers>
2022-10-20 09:56:27 -0400

    activate with fallback paths in root fs

--- modules/envfs.nix
+++ modules/envfs.nix
@@ -8,8 +8,8 @@ let
       options = [
         "fallback-path=${pkgs.runCommand "fallback-path" {} ''
           mkdir -p $out
-          ln -s ${pkgs.coreutils}/bin/env $out/env
-          ln -s ${config.system.build.binsh}/bin/sh $out/sh
+          ln -s ${config.environment.usrbinenv} $out/env
+          ln -s ${config.environment.binsh} $out/sh
         ''}"
       ];
     };
@@ -22,6 +22,17 @@ let
 in {
   environment.systemPackages = [ (pkgs.callPackage ../. {}) ];
   fileSystems = if config.virtualisation ? qemu then lib.mkVMOverride mounts else mounts;
+
+  system.activationScripts.envfsfallback = ''
+    mkdir -p /run/bindroot
+    mount --bind --make-unbindable / /run/bindroot
+    mkdir -m 0755 -p /run/bindroot/usr/bin
+    ln -sfn ${config.environment.usrbinenv} /run/bindroot/usr/bin/env
+    mkdir -m 0755 -p /run/bindroot/bin
+    ln -sfn ${config.environment.binsh} /run/bindroot/bin/sh
+    umount /run/bindroot
+    rmdir /run/bindroot
+  '';
 
   system.activationScripts.usrbinenv = lib.mkForce "";
   system.activationScripts.binsh = lib.mkForce "";
